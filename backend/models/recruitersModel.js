3daf11ae8c9b3f7571ccc78cef12d7024ac2c72b
shruti patel <54548457+shrutipatel0812@users.noreply.github.com>
2021-06-11 22:58:23 +0530

    Add files via upload

--- backend/models/recruitersModel.js
+++ backend/models/recruitersModel.js
@@ -1,17 +1,17 @@
 const mongoose = require("mongoose");
 
 const recruitersSchema= new mongoose.Schema({
-    passwordHash:{type:"String",required: true},
-    name:{type:"String",required: true},
-    domain:{type:"String",required: true},
-    address:{type:"String",required: true},
-    aboutUs:{type:"String",required: true},
-    email:{type:"String",required: true},
-    website:{type:"String",required: true},
-    officialContact:{type:"String",required: true},
-    nameHR:{type:"String",required: true},
-    emailHR:{type:"String",required: true},
-    phoneNo:{type:"String",required: true},
+    passwordHash:{type:"String"},
+    name:{type:"String"},
+    domain:{type:"String"},
+    address:{type:"String"},
+    aboutUs:{type:"String"},
+    email:{type:"String"},
+    website:{type:"String"},
+    officialContact:{type:"String"},
+    nameHR:{type:"String"},
+    emailHR:{type:"String"},
+    phoneNo:{type:"String"},
     
 })
 
--- backend/models/studentsModels.js
+++ backend/models/studentsModels.js
@@ -1,19 +1,19 @@
 const mongoose = require("mongoose");
 
 const studentsSchema= new mongoose.Schema({
-    passwordHash:{type:"String",required: true},
-    firstName:{type:"String",required: true},
-    lastName:{type:"String",required: true},
-    course:{type:"String",required: true},
-    collage:{type:"String",required: true},
-    address:{type:"String",required: true},
+    passwordHash:{type:"String"},
+    firstName:{type:"String"},
+    lastName:{type:"String"},
+    course:{type:"String"},
+    collage:{type:"String"},
+    address:{type:"String"},
     aboutMe:{type:"String"},
-    email:{type:"String",required: true},
+    email:{type:"String"},
     website:{type:"String"},
-    role:{type:"String",required: true},
-    workExperience:{type:"String",required: true},
-    dob:{type:"String",required: true},
-    phoneNo:{type:"String",required: true},
+    role:{type:"String"},
+    workExperience:{type:"String"},
+    dob:{type:"String"},
+    phoneNo:{type:"String"},
 })
 
 module.exports= mongoose.model("Students" ,studentsSchema)
